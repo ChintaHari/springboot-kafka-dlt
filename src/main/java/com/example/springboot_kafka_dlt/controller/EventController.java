868a335a7f203df928f5fcb055970029db549c9b
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-08-14 12:44:19 -0500

    Controller

---
+++ src/main/java/com/example/springboot_kafka_dlt/controller/EventController.java
@@ -0,0 +1,36 @@
+package com.example.springboot_kafka_dlt.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.springboot_kafka_dlt.dto.User;
+import com.example.springboot_kafka_dlt.publisher.KafkaMessagePublisher;
+import com.example.springboot_kafka_dlt.util.CsvReaderUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher publisher;
+
+
+    @PostMapping("/publishNew")
+    public ResponseEntity<?> publishEvent(@RequestBody User user) {
+        try {
+            // List<User> users = CsvReaderUtils.readDataFromCsv();
+            // users.forEach(usr -> publisher.sendEvents(usr));
+            publisher.sendEvents(user);
+            return ResponseEntity.ok("Message published successfully");
+        } catch (Exception exception) {
+            return ResponseEntity.
+                    status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+}
